672234b483610e2943cd292fee7df3fe8c0506e4
Vijay Tripuraneni <v_tripuraneni@hotmail.com>
2025-03-06 00:12:07 -0800

    fix region

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "region" {
   type        = string
   description = "(Optional) AWS Region to deploy in. Defaults to us-east-1."
-  default     = "us-east-1"
+  default     = "us-west-2"
 }
 
 variable "prefix" {
