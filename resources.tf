fd7c347fa64aab4017636cf04bc50c2661a41164
Vijay Tripuraneni <v_tripuraneni@hotmail.com>
2025-03-06 12:17:59 -0800

    path fix

--- resources.tf
+++ resources.tf
@@ -43,7 +43,7 @@ resource "aws_instance" "main" {
   })
 
   user_data_replace_on_change = true
-  user_data = templatefile("/templates/userdata.sh", {
+  user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
   })
 }
