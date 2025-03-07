160155afb0bb960c071cf854e61baec5326b6129
Vijay Tripuraneni <v_tripuraneni@hotmail.com>
2025-03-06 20:22:37 -0800

    Switch to using secrets manager

--- datasources.tf
+++ datasources.tf
@@ -2,11 +2,11 @@
 # DATA SOURCES
 ##################################################################################
 
-data "aws_ssm_parameter" "amzn2_linux" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
-
-data "tfe_outputs" "Networking" {
+data "tfe_outputs" "networking" {
   organization = var.tfe_organization
   workspace    = var.tfe_workspace_name
+}
+
+data "aws_ssm_parameter" "amzn2_linux" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,8 @@ terraform {
       version = "~>5.0"
     }
     tfe = {
-      source = "hashicorp/tfe"
+      source  = "hashicorp/tfe"
+      version = "~> 0.0"
     }
-
   }
 }
